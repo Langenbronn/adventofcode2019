d8044ab1b7e443f804a47c968e8ed9118969f279
Arthur <arthur.langenbronn@gmail.com>
2020-01-12 23:20:51 +0100

    Advendofcode : day2

--- src/main.java
+++ src/main.java
@@ -4,6 +4,6 @@ import adventofcode.module.FuelCalcul;
 public class main {
     public static void main(String[] args) {
         FuelCalcul fuelCalcul = new FuelCalcul();
-        System.out.println(fuelCalcul.fuelRequired("modules.txt"));
+        System.out.println(fuelCalcul.fuelRequired("modules"));
     }
 }
