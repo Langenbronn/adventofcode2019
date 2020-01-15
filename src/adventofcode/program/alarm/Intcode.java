edaef1175909cc55f6e177e6ab252443eb6e321f
Arthur <arthur.langenbronn@gmail.com>
2020-01-16 00:07:17 +0100

    Advendofcode : day2  Convert list String to list Integer

--- src/adventofcode/program/alarm/Intcode.java
+++ src/adventofcode/program/alarm/Intcode.java
@@ -5,22 +5,44 @@ import adventofcode.file.read.FileReadData;
 import java.util.ArrayList;
 
 public class Intcode {
-    private ArrayList<String> list = new ArrayList<String>();
+    private ArrayList<Integer> listIntcode = new ArrayList<Integer>();
 
     public ArrayList<String> intcodeList(String source) {
         FileReadData fRead = new FileReadData("programAlarm");
+        ArrayList<String> list = new ArrayList<String>();
         fRead.readFileByLine(list);
-        System.out.println("list : "+ list);
+        listIntcode = getIntegerArray(list);
         int sequence = 0;
         while (sequence == 5) {
-            list.get(sequence*4);
+            opcodeSequence(sequence*4);
             sequence++;
         }
 
         return list;
     }
 
-    public void opcode(){
+    private void opcodeSequence(int opcode) {
+        switch (listIntcode.get(opcode)) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 99:
+                break;
+            default:
+                break;
+        }
+    }
 
+    private ArrayList<Integer> getIntegerArray(ArrayList<String> stringArray) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        for(String stringValue : stringArray) {
+            try {
+                result.add(Integer.parseInt(stringValue));
+            } catch(NumberFormatException nfe) {
+                System.out.println("Could not parse " + nfe);
+            }
+        }
+        return result;
     }
 }
