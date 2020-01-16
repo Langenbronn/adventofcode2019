263cea8d6dc763e945f38abec09d693487fdd141
Arthur <arthur.langenbronn@gmail.com>
2020-01-16 22:09:32 +0100

    Advendofcode : day2

--- src/adventofcode/program/alarm/Intcode.java
+++ src/adventofcode/program/alarm/Intcode.java
@@ -13,26 +13,45 @@ public class Intcode {
         fRead.readFileByLine(list);
         listIntcode = getIntegerArray(list);
         int sequence = 0;
-        while (sequence == 5) {
-            opcodeSequence(sequence*4);
+        int opcode = 0;
+        while (opcode != 99) {
+            System.out.println("sequence " + sequence);
+            System.out.println("change " + listIntcode);
+            opcode = opcodeSequence(sequence*4);
+            System.out.println("opcode " + opcode);
             sequence++;
         }
 
         return list;
     }
 
-    private void opcodeSequence(int opcode) {
+    private int opcodeSequence(int opcode) {
+//        int op0 = listIntcode.get(opcode);
+//        int op1 = listIntcode.get(opcode + 1);
+//        int op2 = listIntcode.get(opcode + 2);
+//        int op3 = listIntcode.get(opcode + 3);
+//        int op11 = listIntcode.get(op1);
+//        int op22 = listIntcode.get(op2);
+//        int op33 = listIntcode.get(op3);
         switch (listIntcode.get(opcode)) {
             case 1:
+                listIntcode.set(opcode+3, listofList(opcode+1) + listofList(opcode+2));
                 break;
             case 2:
+                listIntcode.set(opcode+3, listofList(opcode+1) * listofList(opcode+2));
                 break;
             case 99:
                 break;
             default:
                 break;
         }
+        return listIntcode.get(opcode);
     }
+
+    private int listofList(int n) {
+        return listIntcode.get(listIntcode.get(n));
+    }
+
 
     private ArrayList<Integer> getIntegerArray(ArrayList<String> stringArray) {
         ArrayList<Integer> result = new ArrayList<Integer>();
