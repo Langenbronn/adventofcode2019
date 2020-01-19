70253cb31173bc8f9b12265fe290d69a9c958700
Arthur <arthur.langenbronn@gmail.com>
2020-01-19 15:28:20 +0100

    Advendofcode : boucle Ok

--- src/adventofcode/program/location/WireLocalisation.java
+++ src/adventofcode/program/location/WireLocalisation.java
@@ -5,42 +5,47 @@ import adventofcode.file.read.FileReadData;
 import java.util.ArrayList;
 
 public class WireLocalisation {
-    private ArrayList<ArrayList<Dot>> listCoordonate = new ArrayList<ArrayList<Dot>>();
+    private ArrayList<ArrayList<Dot>> listCoordonate = new ArrayList();
     private int x = 0;
     private int y = 0;
 
-    public void convertWireToCoord(String source)  {
+    public void convertWireToCoord(String source) {
         FileReadData fRead = new FileReadData(source);
-        ArrayList<ArrayList<String>>  list = new ArrayList ();
+        ArrayList<ArrayList<String>> list = new ArrayList();
         fRead.readFileListByLine(list);
         System.out.println("list " + list);
 
-        listCoordonate.add(new ArrayList<Dot>());
-        listOfListDot(0,x,y);
-        for(String wire : list.get(0)) {
-            System.out.println("wire " + wire);
-            readCoord(0, wire);
+        int num = 0;
+        for (ArrayList<String> lC : list) {
+            x = 0;
+            y = 0;
+            listCoordonate.add(new ArrayList());
+            listOfListDot(num, x, y);
+            for (String wire : lC) {
+                readCoord(num, wire);
+            }
+            num++;
         }
         System.out.println("listCoordonate " + listCoordonate);
     }
 
-    public void readCoord (int num, String wire){
+    public void readCoord(int num, String wire) {
         switch (wire.charAt(0)) {
             case 'U':
                 y = y + Integer.parseInt(wire.substring(1));
-                listOfListDot(num,x,y);
+                listOfListDot(num, x, y);
                 break;
             case 'D':
                 y = y - Integer.parseInt(wire.substring(1));
-                listOfListDot(num,x,y);
+                listOfListDot(num, x, y);
                 break;
             case 'R':
                 x = x + Integer.parseInt(wire.substring(1));
-                listOfListDot(num,x,y);
+                listOfListDot(num, x, y);
                 break;
             case 'L':
                 x = x - Integer.parseInt(wire.substring(1));
-                listOfListDot(num,x,y);
+                listOfListDot(num, x, y);
                 break;
             default:
                 System.out.println("error");
@@ -49,6 +54,6 @@ public class WireLocalisation {
     }
 
     private void listOfListDot(int num, int x, int y) {
-        listCoordonate.get(num).add((new Dot (x,y)));
+        listCoordonate.get(num).add((new Dot(x, y)));
     }
 }
