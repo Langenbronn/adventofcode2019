685e4d3023ba249bf1b2d6bd8d2d85249938624a
Arthur <arthur.langenbronn@gmail.com>
2020-01-24 00:18:28 +0100

    Advendofcode : 4Days Begin

--- src/adventofcode/program/secure/Password.java
+++ src/adventofcode/program/secure/Password.java
@@ -1,6 +1,12 @@
 package adventofcode.program.secure;
 
+import java.util.ArrayList;
+
 public class Password {
 
+   public int passwordPossibility(ArrayList<Integer> puzzle) {
+       int possibility = 0;
 
+       return possibility;
+    }
 }
